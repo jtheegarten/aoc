40f18dde93382cfbfdbbd03ec2b8c7f4da702de4
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-01 10:43:08 +0100

    *shrug*

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return generateElvesList(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return generateElvesList(input).sortedDescending().subList(0, 3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+private fun generateElvesList(input: List<String>): List<Int> {
+    val elves = mutableListOf<Int>()
+    val iter = input.iterator()
+
+    var elfSum = 0
+    while (iter.hasNext()) {
+        val line = iter.next()
+        if (line.isEmpty()) {
+            elves.add(elfSum)
+            elfSum = 0
+        } else {
+            elfSum += line.toInt()
+        }
+    }
+    return elves
 }
