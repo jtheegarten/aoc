7a01496603d3f697098dbb8dd61d2645894bf719
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-01 11:23:31 +0100

    Don't pollute the whole package

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,22 @@
 fun main() {
+
+    fun generateElvesList(input: List<String>): List<Int> {
+        val elves = mutableListOf<Int>()
+        val iter = input.iterator()
+
+        var elfSum = 0
+        while (iter.hasNext()) {
+            val line = iter.next()
+            if (line.isEmpty()) {
+                elves.add(elfSum)
+                elfSum = 0
+            } else {
+                elfSum += line.toInt()
+            }
+        }
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
         return generateElvesList(input).max()
     }
@@ -10,21 +28,4 @@ fun main() {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
-}
-
-private fun generateElvesList(input: List<String>): List<Int> {
-    val elves = mutableListOf<Int>()
-    val iter = input.iterator()
-
-    var elfSum = 0
-    while (iter.hasNext()) {
-        val line = iter.next()
-        if (line.isEmpty()) {
-            elves.add(elfSum)
-            elfSum = 0
-        } else {
-            elfSum += line.toInt()
-        }
-    }
-    return elves
 }
