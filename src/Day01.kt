2c27b18b2cb9ee5ec04749f96c902c92d15e83d1
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-02 12:31:59 +0100

    Reworked day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,31 +1,24 @@
+import java.io.File
+
 fun main() {
 
-    fun generateElvesList(input: List<String>): List<Int> {
-        val elves = mutableListOf<Int>()
-        val iter = input.iterator()
-
-        var elfSum = 0
-        while (iter.hasNext()) {
-            val line = iter.next()
-            if (line.isEmpty()) {
-                elves.add(elfSum)
-                elfSum = 0
-            } else {
-                elfSum += line.toInt()
+    fun generateElvesList(input: String): List<Int> =
+        input
+            .split("\n\n")
+            .map { elf ->
+                elf.split("\n")
+                    .sumOf { it.toIntOrNull() ?: 0 }
             }
-        }
-        return elves
-    }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: String): Int {
         return generateElvesList(input).max()
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: String): Int {
         return generateElvesList(input).sortedDescending().take(3).sum()
     }
 
-    val input = readInput("Day01")
+    val input = File("src/Day01.txt").readText()
     println("Part 1: ${part1(input)}")
     println("Part 2: ${part2(input)}")
 }
