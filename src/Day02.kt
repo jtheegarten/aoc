3d273b7211aabf3ff7cca588fdd0ebd5ce2b211f
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-02 10:45:35 +0100

    Some cleanup

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,35 +1,17 @@
 import RPS.PAPER
 import RPS.ROCK
 import RPS.SCISSORS
+import Result.DRAW
+import Result.LOSS
+import Result.WIN
 
 fun main() {
 
-    fun result(opponent: RPS, self: RPS) =
-        when (opponent) {
-            ROCK -> when (self) {
-                ROCK -> draw
-                PAPER -> win
-                SCISSORS -> loss
-            }
-
-            PAPER -> when (self) {
-                ROCK -> loss
-                PAPER -> draw
-                SCISSORS -> win
-            }
-
-            SCISSORS -> when (self) {
-                ROCK -> win
-                PAPER -> loss
-                SCISSORS -> draw
-            }
-        }
-
     fun List<String>.score() =
-        component2().toRPS().score + result(component1().toRPS(), component2().toRPS())
+        component2().toRPS().score + component2().toRPS().play(component1().toRPS()).value
 
     fun List<String>.scorePredicting() =
-        component2().toResult() + component2().toResult().played(component1().toRPS())
+        component2().toResult().value + component2().toResult().played(component1().toRPS())
 
     fun part1(input: List<String>): Int =
         input.sumOf { it.split(" ").score() }
@@ -44,40 +26,65 @@ fun main() {
 
 }
 
-const val loss = 0
-const val draw = 3
-const val win = 6
-
 fun String.toResult() =
     when (this) {
-        "X" -> loss
-        "Y" -> draw
-        "Z" -> win
-        else -> loss
+        "X" -> LOSS
+        "Y" -> DRAW
+        "Z" -> WIN
+        else -> LOSS
     }
 
-fun Int.played(opponent: RPS) =
+fun Result.played(opponent: RPS) =
     when(this) {
-        0 -> when(opponent) {
+        LOSS -> when(opponent) {
             ROCK -> SCISSORS.score
             PAPER -> ROCK.score
             SCISSORS -> PAPER.score
         }
-        3 -> opponent.score
-        6 -> when(opponent) {
+        DRAW -> opponent.score
+        WIN -> when(opponent) {
             ROCK -> PAPER.score
             PAPER -> SCISSORS.score
             SCISSORS -> ROCK.score
         }
-        else -> 0
     }
 
 fun String.toRPS() = RPS.fromCode(this)
+
+enum class Result(val value: Int) {
+    WIN(6),
+    DRAW(3),
+    LOSS(0);
+
+    companion object {
+        fun fromResult(result: Int) =
+            enumValues<Result>().single { it.value == result }
+    }
+}
 
 enum class RPS(val code: List<String>, val score: Int) {
     ROCK(listOf("A", "X"), 1),
     PAPER(listOf("B", "Y"), 2),
     SCISSORS(listOf("C", "Z"), 3);
+
+    fun play(opponent: RPS) =
+        when(this) {
+            ROCK -> when(opponent) {
+                ROCK -> DRAW
+                PAPER -> LOSS
+                SCISSORS -> WIN
+            }
+            PAPER -> when(opponent) {
+                ROCK -> WIN
+                PAPER -> DRAW
+                SCISSORS -> LOSS
+            }
+            SCISSORS -> when(opponent) {
+                ROCK -> LOSS
+                PAPER -> WIN
+                SCISSORS -> DRAW
+            }
+        }
 
     companion object {
         fun fromCode(code: String) =
