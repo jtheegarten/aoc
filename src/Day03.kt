d9abea3deab0b0034b5fe38bd58ba5bfaa50c1dd
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2022-12-03 12:29:30 +0100

    Day 03 done

---
+++ src/Day03.kt
@@ -0,0 +1,27 @@
+import java.io.File
+
+fun main() {
+
+    fun part1(input: List<String>): Int =
+        input.flatMap { it.duplicates() }.sumOf { it.score() }
+
+    fun part2(input: List<String>): Int =
+        input.chunked(3).flatMap { it.badge() }.sumOf { it.score() }
+
+    val input = File("src/Day03.txt").readLines()
+//    require(part1(input) == 157)
+//    require(part2(input) == 70)
+
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+
+}
+
+fun List<String>.badge() = map { it.toSet() }.reduce{a, b -> a.intersect(b)}
+
+fun String.duplicates() =
+    chunked(size = (length / 2)).map { it.toSet() }.reduce{a, b -> a.intersect(b)}
+
+fun Char.score() = valueMap[this] ?: 0
+
+val valueMap = (('a'..'z').plus('A'..'Z')).zip(1..52).toMap()
