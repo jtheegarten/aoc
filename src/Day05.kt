2b84989d129f78440ecd70c1466043287ba6d81c
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-05 09:50:41 +0100

    Meh, it works

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,19 +1,51 @@
 import java.io.File
+import java.util.Stack
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun addLine(result: MutableMap<Int, Stack<String>>, line: String) {
+        var position = 1
+        while (position < line.length) {
+            val char = line[position].toString()
+            if (char.isNotBlank()) result[(position + 3) / 4]!!.push(char)
+            position += 4
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun parseStacks(stacksDescription: String): Map<Int, Stack<String>> {
+        val lines = stacksDescription.lines().reversed()
+        val result = mutableMapOf<Int, Stack<String>>()
+        for (i in 1..(lines.first().last().digitToInt())) {
+            result[i] = Stack<String>()
+        }
+        for (i in 1 until lines.size) {
+            addLine(result, lines[i])
+        }
+
+        return result
     }
 
-    val input = File("src/Day05_test.txt").readLines()
-//    require(part1(input) == 157)
-//    require(part2(input) == 70)
+    fun applyMove(move: String, stacks: Map<Int, Stack<String>>, stableOrder: Boolean = false) {
+        val (amount, from, to) = Regex("(\\d+)").findAll(move).toList().map { it.value.toInt() }
+        if (!stableOrder) {
+            repeat(amount) { stacks[to]!!.push(stacks[from]!!.pop()) }
+        } else {
+            List(amount) {stacks[from]!!.pop()}.reversed().forEach{ stacks[to]!!.push(it) }
+        }
+    }
 
-    println("Part 1: ${part1(input)}")
-    println("Part 2: ${part2(input)}")
+    fun solve(input: List<String>, stableOrder: Boolean = false): String {
+        val (stacksDescription, instructionsDescription) = input
+        val stacks = parseStacks(stacksDescription)
+        instructionsDescription.lines().filter { it.isNotEmpty() }.forEach { applyMove(it, stacks, stableOrder) }
+
+        return stacks.values.joinToString("") { it.pop() }
+    }
+
+    val input = File("src/Day05.txt").readText().split("\n\n")
+//    require(solve(input) == "CMZ")
+//    require(solve(input, true) == "MCD")
+
+    println("Part 1: ${solve(input)}")
+    println("Part 2: ${solve(input, true)}")
 }
