d0095a6d733b9c780792090480a82de088698350
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-06 12:29:56 +0100

    IndexOfFirst, baby!

--- src/Day06.kt
+++ src/Day06.kt
@@ -9,4 +9,4 @@ fun main() {
     Day06().run()
 }
 
-private fun String.indexOfMarker(length: Int = 4) = windowed(length, 1).map { it.toSet().size == length }.indexOf(true) + length
+private fun String.indexOfMarker(length: Int = 4) = windowed(length, 1).indexOfFirst { it.toSet().size == length } + length
