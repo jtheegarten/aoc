5e0021f40ac258ed9005e8f3288467624c9046a4
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-07 12:04:56 +0100

    Bit of refactoring

--- src/Day07.kt
+++ src/Day07.kt
@@ -4,41 +4,29 @@ const val maxSpace: Long = 70_000_000
 const val spaceNeeded: Long = 30_000_000
 
 class Day07 : Day<Long>("07", 95437, 24933642) {
-    override fun part1(input: List<String>): Long = input.iterator().buildTree().sumOfFoldersWithSizeAtMost(100000)
+    override fun part1(input: List<String>): Long = input.drop(1).iterator().buildTree().sumOfFoldersWithSizeAtMost(100000)
 
-    override fun part2(input: List<String>): Long {
-        val root = input.iterator().buildTree()
-        val deletionTarget = (spaceNeeded - (maxSpace - root.size)).absoluteValue
-        val directoriesList = root.listDeletableDirectories(deletionTarget)
-        return directoriesList.minBy { it.size }.size
-    }
+    override fun part2(input: List<String>): Long = input.drop(1).iterator().buildTree()
+        .let { root -> root.listDeletableDirectories((spaceNeeded - (maxSpace - root.size)).absoluteValue).minBy { it.size }.size }
 
 
 }
-
-private fun Folder.listDeletableDirectories(deletionTarget: Long): List<Folder> =
-    if (size >= deletionTarget) listOf(this) + deletableSubdirectories(deletionTarget)
-    else deletableSubdirectories(deletionTarget)
-
-private fun Folder.deletableSubdirectories(spaceNeeded: Long) = content.filterIsInstance<Folder>().flatMap { it.listDeletableDirectories(spaceNeeded) }
 
 fun main() {
     Day07().run()
 }
 
-fun Folder.sumOfFoldersWithSizeAtMost(size: Long): Long =
-    if (this.size <= size) this.size + sumOfSubFolders(size)
-    else sumOfSubFolders(size)
+private fun Folder.listDeletableDirectories(deletionTarget: Long): List<Folder> = deletableSubdirectories(deletionTarget) + if (size >= deletionTarget) listOf(this) else listOf()
+
+private fun Folder.deletableSubdirectories(spaceNeeded: Long) = content.filterIsInstance<Folder>().flatMap { it.listDeletableDirectories(spaceNeeded) }
+
+private fun Folder.sumOfFoldersWithSizeAtMost(size: Long): Long = sumOfSubFolders(size) + if(this.size <= size) this.size else 0
 
 private fun Folder.sumOfSubFolders(size: Long) = content.filterIsInstance<Folder>().sumOf { it.sumOfFoldersWithSizeAtMost(size) }
 
-fun Iterator<String>.buildTree(): Folder {
-    next()
-    return Folder("/").applyInstructions(this)
-}
+private fun Iterator<String>.buildTree(): Folder = Folder().applyInstructions(this)
 
-
-class Folder(val name: String, val content: MutableList<FSNode> = mutableListOf()) : FSNode {
+private class Folder(val content: MutableList<FSNode> = mutableListOf()) : FSNode {
     override val size: Long
         get() = content.sumOf { it.size }
 
@@ -51,23 +39,17 @@ class Folder(val name: String, val content: MutableList<FSNode> = mutableListOf(
         return this
     }
 
-    private fun applyInstruction(instructions: Iterator<String>): Boolean {
-        val instruction = instructions.next()
+    private fun applyInstruction(instructions: Iterator<String>) = instructions.next().let { instruction ->
         when {
             instruction == "$ cd .." -> return true
-            instruction.startsWith("$ cd") -> Folder(instruction.substring(5)).applyInstructions(instructions).let { content.add(it) }
-            instruction[0].isDigit() -> instruction.split(" ").let { (size, name) -> content.add(File(name, size.toLong())) }
+            instruction.startsWith("$ cd") -> Folder().applyInstructions(instructions).let { content.add(it) }.also { return false }
+            instruction[0].isDigit() -> instruction.split(" ").let { content.add(File(it.first().toLong())) }.also { return false }
             else -> return false
         }
-        return false
     }
-
-    override fun toString() = "Folder $name of size $size with content:\n\n" + content.joinToString("\n") { " - \t ${it.toString()}" }
 }
 
-class File(val name: String, override val size: Long) : FSNode {
-    override fun toString() = "File $name of size $size"
-}
+private class File(override val size: Long) : FSNode
 
 interface FSNode {
     val size: Long
