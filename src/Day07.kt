9939b4752c73d7b4b03bb2b541a341cac06f80e6
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2022-12-08 20:10:16 +0100

    Day 07 refactor

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,10 +2,10 @@ const val maxSpace: Long = 70_000_000
 const val spaceNeeded: Long = 30_000_000
 
 class Day07 : Day<Long>(95437, 24933642) {
-    override fun part1(input: List<String>): Long = input.drop(1).iterator().buildTree().getAllFolders().map { it.size }.filter { it <= 100_000 }.sum()
+    override fun part1(input: List<String>): Long = input.buildTree().getAllFolders().map { it.size }.filter { it <= 100_000 }.sum()
 
     override fun part2(input: List<String>): Long {
-        val root = input.drop(1).iterator().buildTree()
+        val root = input.buildTree()
         val deletionTarget = (spaceNeeded - (maxSpace - root.size))
         return root.getAllFolders().filter { it.size >= deletionTarget }.minOf { it.size }
     }
@@ -17,7 +17,7 @@ fun main() {
 
 private fun Folder.getAllFolders(): List<Folder> = listOf(this) + content.filterIsInstance<Folder>().flatMap { it.getAllFolders() }
 
-private fun Iterator<String>.buildTree(): Folder = Folder().applyInstructions(this)
+private fun List<String>.buildTree(): Folder = drop(1).iterator().let(Folder()::applyInstructions)
 
 private class Folder(val content: MutableList<FSNode> = mutableListOf()) : FSNode {
     override val size: Long
@@ -25,9 +25,7 @@ private class Folder(val content: MutableList<FSNode> = mutableListOf()) : FSNod
 
     fun applyInstructions(instructions: Iterator<String>): Folder {
         while (instructions.hasNext()) {
-            if (applyInstruction(instructions)) {
-                return this
-            }
+            if (applyInstruction(instructions)) return this
         }
         return this
     }
@@ -36,10 +34,12 @@ private class Folder(val content: MutableList<FSNode> = mutableListOf()) : FSNod
         when {
             instruction == "$ cd .." -> return true
             instruction.startsWith("$ cd") -> Folder().applyInstructions(instructions).let { content.add(it) }.also { return false }
-            instruction[0].isDigit() -> instruction.split(" ").let { content.add(File(it.first().toLong())) }.also { return false }
+            instruction[0].isDigit() -> addFile(instruction.split(" ").first()).also { return false }
             else -> return false
         }
     }
+
+    private fun addFile(size: String) = content.add(File(size.toLong()))
 }
 
 private class File(override val size: Long) : FSNode
