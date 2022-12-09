22ee82902c598694a62e06cdfe1a9c38509179f7
Jon Theegarten <jon.theegarten@iits-consulting.de>
2022-12-09 16:37:25 +0100

    Refactor Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,17 +1,15 @@
 import java.io.File
 
+class Day04: Day<Int>(2, 4) {
+
+    override fun part1(input: List<String>): Int = input.count { it.fullyContained() }
+
+    override fun part2(input: List<String>): Int = input.count { it.overlap() }
+
+}
+
 fun main() {
-
-    fun part1(input: List<String>): Int = input.count { it.fullyContained() }
-
-    fun part2(input: List<String>): Int = input.count { it.overlap() }
-
-    val input = File("src/Day04.txt").readLines()
-//    require(part1(input) == 2)
-//    require(part2(input) == 4)
-
-    println("Part 1: ${part1(input)}")
-    println("Part 2: ${part2(input)}")
+    Day04().run()
 }
 
 fun String.fullyContained() = split(",").map { it.toNumberList() }
