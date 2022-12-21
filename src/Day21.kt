0baf2971bac145d05862f492b007aa898457f36f
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2022-12-21 10:58:45 +0100

    We had to go to double...

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,15 +1,16 @@
 import MathOperation.*
 import MathOperation.Companion.fromSign
+import kotlin.math.absoluteValue
 
 fun main() {
     Day21().run()
 }
 
-class Day21 : Day<Long>(152, 19) {
+class Day21 : Day<Long>(152, 301) {
 
-    override fun part1(input: List<String>): Long = input.toMonkey().value
+    override fun part1(input: List<String>): Long = input.toMonkey().value.toLong()
 
-    override fun part2(input: List<String>): Long = 19
+    override fun part2(input: List<String>): Long = input.humanNumber()
 }
 
 
@@ -18,6 +19,34 @@ private fun List<String>.toMonkey(): MonkeyNode = this
     .associate { it.first() to it[1] }
     .let { MathMonkey.from(it["root"]!!, it) }
 
+private fun List<String>.humanNumber(): Long {
+    val inputMap = this.map { it.split(": ") }.associate { it.first() to it[1] }
+    var max = Long.MAX_VALUE
+    var min = 1L
+
+    while (true) {
+        val halfDiff = max - (max - min) / 2
+
+        val tests = listOf(min, halfDiff, max).map { checkValue ->
+            val checkMap = inputMap.toMutableMap()
+            checkMap["humn"] = checkValue.toString()
+            (MathMonkey.from(checkMap["root"]!!, checkMap) as MathMonkey)
+                .let {
+                    checkValue to (it.left diff it.right)
+                }
+
+        }.sortedBy { it.second }
+
+        val result = tests.firstOrNull { it.second == 0.0 }
+        if (result != null) {
+            return result.first
+        } else {
+            min = tests.take(2).minOf { it.first }
+            max = tests.take(2).maxOf { it.first }
+        }
+    }
+
+}
 
 private data class MathMonkey(val op: MathOperation, val left: MonkeyNode, val right: MonkeyNode) : MonkeyNode {
 
@@ -25,21 +54,23 @@ private data class MathMonkey(val op: MathOperation, val left: MonkeyNode, val r
 
     companion object {
         fun from(instruction: String, instructionsMap: Map<String, String>): MonkeyNode =
-            if (instruction.toLongOrNull() != null) ValueMonkey(instruction.toLong())
+            if (instruction.toLongOrNull() != null) ValueMonkey(instruction.toDouble())
             else instruction.split(" ")
                 .let { MathMonkey(fromSign(it[1]), from(instructionsMap[it[0]]!!, instructionsMap), from(instructionsMap[it[2]]!!, instructionsMap)) }
     }
 }
 
-private data class ValueMonkey(override val value: Long) : MonkeyNode
+private data class ValueMonkey(override val value: Double) : MonkeyNode
 
 private interface MonkeyNode {
-    val value: Long
+    val value: Double
 
-    infix operator fun plus(other: MonkeyNode) = this.value + other.value
-    infix operator fun minus(other: MonkeyNode) = this.value - other.value
-    infix operator fun div(other: MonkeyNode) = this.value / other.value
-    infix operator fun times(other: MonkeyNode) = this.value * other.value
+    infix operator fun plus(other: MonkeyNode) = value + other.value
+    infix operator fun minus(other: MonkeyNode) = value - other.value
+    infix operator fun div(other: MonkeyNode) = value / other.value
+    infix operator fun times(other: MonkeyNode) = value * other.value
+    infix fun equals(other: MonkeyNode) = value == other.value
+    infix fun diff(other: MonkeyNode) = (value - other.value).absoluteValue
 }
 
 private fun MathOperation.execute(left: MonkeyNode, right: MonkeyNode) =
