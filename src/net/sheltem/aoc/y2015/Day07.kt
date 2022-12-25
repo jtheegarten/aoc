d821cad89ef9052977956d0e8d396511f3cd1435
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2022-12-25 13:36:31 +0100

    2015: Day 07 In a much simpler way

--- src/net/sheltem/aoc/y2015/Day07.kt
+++ src/net/sheltem/aoc/y2015/Day07.kt
@@ -1,70 +1,68 @@
 package net.sheltem.aoc.y2015
 
-import net.sheltem.aoc.y2015.LogicOperation.*
-
 fun main() {
     Day07().run()
 }
 
-class Day07 : Day<Int>(72, 0) {
-    override fun part1(input: List<String>): Int = solve(input.parseInput())
+class Day07 : Day<Int>(72, 72) {
+    override fun part1(input: List<String>): Int = solve(input)
 
 
-    override fun part2(input: List<String>): Int = TODO()
+    override fun part2(input: List<String>): Int = solve(input, 956)
 
 
-    private fun solve(input: Pair<MutableMap<String, UShort>, MutableMap<String, LogicInstruction>>): Int {
-        val valuesMap = input.first
-        val instructionsMap = input.second
+    private fun solve(input:List<String>, overrideB: Int? = null): Int {
+        var workList = if (overrideB!= null) input.map { if (it.endsWith(" -> b")) "$overrideB -> b" else it } else input
 
-        while (!valuesMap.containsKey("a")) {
-            for (instruction in instructionsMap) {
-                val operandValues = instruction.value.operands.map { it to valuesMap[it] }
-                if (operandValues.any { it.second == null }) continue
+        while (true) {
 
-                val newValue = when (instruction.value.op) {
-                    AND -> operandValues[0].second!! and operandValues[1].second!!
-                    OR -> operandValues[0].second!! or operandValues[1].second!!
-                    NOT -> operandValues[0].second!!.inv()
-                    LSHIFT -> operandValues[0].second!! shl (operandValues[1].second!!)
-                    RSHIFT -> operandValues[0].second!! shr (operandValues[1].second!!)
+            val valueMap = workList.mapNotNull {line ->
+                val (what, where) = line.split(" -> ")
+                what.toUShortOrNull()?.let { where to it }
+            }.toMap()
+
+            if (valueMap.containsKey("a")) break
+
+            workList = workList.map {
+                var realValues = it
+                for (value in valueMap) {
+                    if (realValues.startsWith("${value.key} ") || realValues.contains(" ${value.key} ") || realValues.endsWith(" ${value.key}"))
+                    realValues = realValues.replace(value.key, value.value.toString())
                 }
-                valuesMap[instruction.key] = newValue
+                realValues
             }
 
-            if (instructionsMap.isEmpty()) break
-        }
+            workList = workList.map {line ->
+                line.split(" -> ").let { (what, where) ->
+                    when {
+                        line.startsWith("NOT") -> {
+                            what.removePrefix("NOT ").toUShortOrNull()?.inv()?.let { "$it -> $where" }?: line
+                        }
+                        line.contains("OR") -> what.split(" OR ").map { it.toUShortOrNull() }.let { (one, two) ->
+                            if (one != null && two != null) "${one or two} -> $where" else line
+                        }
+                        line.contains("AND") -> what.split(" AND ").map { it.toUShortOrNull() }.let { (one, two) ->
+                            if (one != null && two != null) "${one and two} -> $where" else line
+                        }
+                        line.contains("LSHIFT") -> what.split(" LSHIFT ").map { it.toUShortOrNull() }.let { (one, two) ->
+                            if (one != null && two != null) "${one shl two} -> $where" else line
+                        }
+                        line.contains("RSHIFT") -> what.split(" RSHIFT ").map { it.toUShortOrNull() }.let { (one, two) ->
+                            if (one != null && two != null) "${one shr two} -> $where" else line
+                        }
+                        else -> line
+                    }
+                }
 
-        return valuesMap["a"]!!.toInt()
+            }
+            workList = workList.mapNotNull { line ->
+                if (line.split(" -> ").all { it.toUShortOrNull() != null }) null else line
+            }
+
+        }
+        return workList.single { it.endsWith(" a") }.split(" ")[0].toInt()
     }
 }
 
 private infix fun UShort.shl(steps: UShort) = this.toLong().shl(steps.toInt()).toUShort()
 private infix fun UShort.shr(steps: UShort) = this.toLong().shr(steps.toInt()).toUShort()
-
-private fun List<String>.parseInput(): Pair<MutableMap<String, UShort>, MutableMap<String, LogicInstruction>> {
-    val valuesMap = mutableMapOf<String, UShort>()
-    val instructionsMap = mutableMapOf<String, LogicInstruction>()
-    forEach {instruction ->
-        println(instruction)
-        instruction.split(" -> ").let { (what, where) ->
-            when {
-                what.toIntOrNull() != null -> valuesMap[where] = what.toUShort()
-                what.startsWith("NOT") -> instructionsMap[where] = LogicInstruction(NOT, listOf(what.removePrefix("NOT ")))
-                else -> what.split(" ").let { instructionsMap[where] = LogicInstruction(LogicOperation.valueOf(it[1]), listOf(it[0], it[2])) }
-            }
-        }
-    }
-    return valuesMap to instructionsMap
-}
-
-private class LogicInstruction(val op: LogicOperation, val operands: List<String>)
-
-private enum class LogicOperation {
-    AND,
-    OR,
-    NOT,
-    LSHIFT,
-    RSHIFT;
-
-}
