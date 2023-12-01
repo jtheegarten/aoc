9ce29a4e6d65e8a456c32223ca7cba8e2440876b
Jon Theegarten <jon.theegarten@iits-consulting.de>
2023-12-01 10:26:29 +0100

    A better solution

--- src/net/sheltem/aoc/y2023/Day01.kt
+++ src/net/sheltem/aoc/y2023/Day01.kt
@@ -4,9 +4,8 @@ fun main() {
     Day01().run()
 }
 
-private val allDigits = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9,
-    "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9)
 private val digits = mapOf("1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9)
+private val allDigits = digits + mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
 
 class Day01 : Day<Int>(142, 281) {
 
