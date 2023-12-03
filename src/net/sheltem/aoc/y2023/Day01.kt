962f3741aabfce989c64aebb8984db4ceea44b02
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2023-12-03 11:18:20 +0100

    Minor refactor

--- src/net/sheltem/aoc/y2023/Day01.kt
+++ src/net/sheltem/aoc/y2023/Day01.kt
@@ -3,9 +3,8 @@ package net.sheltem.aoc.y2023
 suspend fun main() {
     Day01().run()
 }
-
-private val digits = mapOf("1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9)
-private val allDigits = digits + mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
+private val digits = ('1'..'9').map { it.toString() }.zip(1..9).toMap()
+private val allDigits = digits + listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine").zip(1..9).toMap()
 
 class Day01 : Day<Int>(142, 281) {
 
