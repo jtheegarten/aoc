a17326342aa1db48228cc25186d0625a77043494
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2023-12-03 11:57:33 +0100

    A little more refactoring

--- src/net/sheltem/aoc/y2023/Day03.kt
+++ src/net/sheltem/aoc/y2023/Day03.kt
@@ -10,7 +10,7 @@ suspend fun main() {
 class Day03 : Day<Long>(4361, 467835) {
 
     override suspend fun part1(input: List<String>): Long {
-        return input.toEngineNumbers().flatten().sum()
+        return input.toEngineNumbers().sum()
     }
 
     override suspend fun part2(input: List<String>): Long {
@@ -20,45 +20,42 @@ class Day03 : Day<Long>(4361, 467835) {
 
 private val numberRegex = Regex("\\d+")
 
-private fun List<String>.toEngineNumbers(): List<List<Long>> = this
-    .mapIndexed { index, line ->
-        val numberMatches = numberRegex.findAll(line)
-        numberMatches.filter { match ->
-            val startIndex = max(match.range.first - 1, 0)
-            val endIndex = min(match.range.last + 1, line.length - 1)
-//            println("${match.range} -> $startIndex - $endIndex")
-            var valid = false
-            val previousRow = this[max(index - 1, 0)].substring(startIndex, endIndex + 1)
-            val nextRow = this[min(index + 1, this.size - 1)].substring(startIndex, endIndex + 1)
-//            println("$previousRow && $nextRow")
-            if (this[index][startIndex].isSymbol()) valid = true
-            if (this[index][endIndex].isSymbol()) valid = true
-            if (previousRow.any { it.isSymbol() }) valid = true
-            if (nextRow.any { it.isSymbol() }) valid = true
+private fun List<String>.toEngineNumbers(): List<Long> = this.mapIndexed { index, line ->
+    val numberMatches = numberRegex.findAll(line)
+    numberMatches.filter { match ->
+        val startIndex = max(match.range.first - 1, 0)
+        val endIndex = min(match.range.last + 2, line.length - 1)
+        val previousRow = this[max(index - 1, 0)].substring(startIndex, endIndex)
+        val thisRow = this[index].substring(startIndex, endIndex)
+        val nextRow = this[min(index + 1, this.size - 1)].substring(startIndex, endIndex)
 
-            valid
-        }.map { it.value.toLong() }
-            .toList()//.also { println("$line -> $it") }
-    }
+        (previousRow + thisRow + nextRow).any { it.isSymbol() }
+
+    }.map { it.value.toLong() }.toList()
+}.flatten()
 
 private fun Char.isSymbol() = !isDigit() && this != '.'
 
 private fun List<String>.toGearRatios(): List<Long> = this
     .mapIndexed { index, line ->
-        val gearSymbolIndexes = Regex("\\*").findAll(line).map { it.range.first }
-//        println(gearSymbolIndexes.toList())
-        gearSymbolIndexes.mapNotNull { gearIndex ->
-            val adjacentNumbers = this.findAdjacentNumbers(index, gearIndex)
-            if (adjacentNumbers.size == 2) {
-                adjacentNumbers.component1() * adjacentNumbers.component2()
-            } else {
-                null
-            }
-        }.toList()
+        Regex("\\*")
+            .findAll(line)
+            .map { it.range.first }
+            .mapNotNull { gearIndex ->
+                val adjacentNumbers = this.findAdjacentNumbers(index, gearIndex)
+                if (adjacentNumbers.size == 2) {
+                    adjacentNumbers.component1() * adjacentNumbers.component2()
+                } else {
+                    null
+                }
+            }.toList()
     }.flatten()
 
-private fun List<String>.findAdjacentNumbers(index: Int, gearIndex: Int): List<Long> {
-    return (numberRegex.findAll(this[index - 1]).filter { gearIndex in (it.range.first - 1)..(it.range.last + 1) } + numberRegex.findAll(this[index])
-        .filter { gearIndex in (it.range.first - 1)..(it.range.last + 1) } + numberRegex.findAll(this[index + 1])
-        .filter { gearIndex in (it.range.first - 1)..(it.range.last + 1) }).toList().map { it.value.toLong() }
-}
+private fun List<String>.findAdjacentNumbers(index: Int, gearIndex: Int): List<Long> =
+    (numberRegex.findAll(this[index - 1]).toList()
+            + numberRegex.findAll(this[index]).toList()
+            + numberRegex.findAll(this[index + 1]).toList())
+        .filter { gearIndex in it.numberRange() }
+        .map { it.value.toLong() }
+
+private fun MatchResult.numberRange() = (range.first - 1)..(range.last + 1)
