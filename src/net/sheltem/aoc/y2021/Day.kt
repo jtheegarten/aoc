65c9cc7f98dbaae586ec16ee55df89678c275846
Jon Theegarten <jon.theegarten@iits-consulting.de>
2023-12-04 13:23:42 +0100

    2021 - Day06

---
+++ src/net/sheltem/aoc/y2021/Day.kt
@@ -0,0 +1,3 @@
+package net.sheltem.aoc.y2021
+
+abstract class Day<T>(part1Test: T, part2Test: T) : net.sheltem.aoc.common.Day<T>(2021, part1Test, part2Test)
---
+++ src/net/sheltem/aoc/y2021/Day06.kt
@@ -0,0 +1,41 @@
+package net.sheltem.aoc.y2021
+
+import java.lang.IllegalStateException
+
+suspend fun main() {
+    Day06().run()
+}
+
+class Day06 : Day<Long>(5934, 26984457539) {
+
+    override suspend fun part1(input: List<String>): Long = input[0]
+        .toFishMap()
+        .let { spawn(80, it) }
+
+    override suspend fun part2(input: List<String>): Long = input[0]
+        .toFishMap()
+        .let { spawn(256, it) }
+}
+
+private fun String.toFishMap() = split(",")
+    .map { it.toInt() }
+    .groupingBy { it }
+    .eachCount()
+    .map { it.key to it.value.toLong() }
+    .toMap()
+
+private fun spawn(repeats: Int, startMap: Map<Int, Long>): Long {
+    var resultMap = startMap
+
+    repeat(repeats) { index ->
+        resultMap = mapOf(8 to (resultMap[0] ?: 0)) + resultMap.map {
+            when (it.key) {
+                0 -> 6 to (it.value + (resultMap[7] ?: 0))
+                in 1..8 -> it.key - 1 to it.value
+                else -> throw IllegalStateException()
+            }
+        }
+    }
+    return resultMap.values.sum()
+
+}
