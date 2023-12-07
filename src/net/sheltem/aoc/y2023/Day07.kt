2468ed4a20a12bf3b6b6effbc7a19baf4a513f03
Jon Theegarten <jon.theegarten@iits-consulting.de>
2023-12-07 14:07:03 +0100

    2023 - Day07 refactor

--- src/net/sheltem/aoc/y2023/Day07.kt
+++ src/net/sheltem/aoc/y2023/Day07.kt
@@ -7,21 +7,23 @@ suspend fun main() {
 class Day07 : Day<Long>(6440, 5905) {
 
     override suspend fun part1(input: List<String>): Long = input.map { line ->
-        line.split(" ").let { PokerHand(it.first(), it.last().toLong()) }
+        line.toPokerHand()
     }.sorted()
-        .mapIndexed { index, hand ->
-            (index + 1) * hand.bid
-        }.sum()
+        .toWinnings()
 
     override suspend fun part2(input: List<String>): Long = input.map { line ->
-        line.split(" ").let { PokerHand(it.first(), it.last().toLong(), true) }
+        line.toPokerHand(true)
     }.sorted()
-        .mapIndexed { index, hand ->
-            (index + 1) * hand.bid
-        }.sum()
+        .toWinnings()
 }
 
-private data class PokerHand(val hand: String, val bid: Long, val jokerRule: Boolean = false) : Comparable<PokerHand> {
+private fun String.toPokerHand(jokerRule: Boolean = false) = this.split(" ").let { PokerHand(it.first(), it.last().toLong(), jokerRule) }
+
+private fun List<PokerHand>.toWinnings(): Long = this.foldIndexed(0) { index, acc, pokerHand ->
+    acc + ((index + 1) * pokerHand.bid)
+}
+
+private data class PokerHand(val hand: String, val bid: Long, val jokerRule: Boolean) : Comparable<PokerHand> {
 
     private val cards = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2').let { if (jokerRule) it - 'J' + 'J' else it }
 
@@ -44,19 +46,16 @@ private data class PokerHand(val hand: String, val bid: Long, val jokerRule: Boo
             handMap.size == 3 && handMap.values.count { it == 2 } == 2 -> 3 // two pair
             handMap.values.count { it == 2 } == 1 -> 2 // pair
             handMap.values.sum() == 5 -> 1 // high card
-            else -> (-1).also { println(hand) }
+            else -> (-1).also { println("Could not determine type of $hand") }
         }
 
-    override fun compareTo(other: PokerHand): Int {
-        return if (this.type - other.type == 0) {
-            this.hand.zip(other.hand).forEach {
-                if (cards.indexOf(it.first) != cards.indexOf(it.second)) {
-                    return cards.indexOf(it.second) - cards.indexOf(it.first)
-                }
-            }.let { return 0 }
+    override fun compareTo(other: PokerHand): Int = (this.type - other.type).let { typeDifference ->
+        if (typeDifference == 0) {
+            this.hand.zip(other.hand).map {
+                cards.indexOf(it.second) - cards.indexOf(it.first)
+            }.first { it != 0 }
         } else {
-            this.type - other.type
+            typeDifference
         }
     }
-
 }
