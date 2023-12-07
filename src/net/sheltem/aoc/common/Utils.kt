f1731f31c62b146d477f76147d607e6dae34a836
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2023-12-07 23:43:54 +0100

    Extracted a generic bfs

--- src/net/sheltem/aoc/common/Utils.kt
+++ src/net/sheltem/aoc/common/Utils.kt
@@ -56,6 +56,7 @@ enum class Direction(val coords: PositionInt) {
         WEST -> NORTH
         else -> NORTH
     }
+
     fun turnLeft() = when (this) {
         NORTH -> WEST
         EAST -> NORTH
@@ -109,3 +110,65 @@ fun Sequence<MatchResult>.toListLong(): List<Long> = toList().toListLong()
 
 
 infix operator fun Pair<Long, Long>.plus(other: Pair<Long, Long>): Pair<Long, Long> = first + other.first to second + other.second
+
+
+class SearchGraph<N>(val nodes: Set<Node<N>>, edges: Set<Edge>, bidirectional: Boolean) {
+
+    private val edges: Set<Edge> = if (bidirectional) edges + edges.map { Edge(it.to, it.from, it.weight) } else edges
+
+    override fun toString() = "nodes: $nodes\nEdges: $edges"
+
+    fun minBfs(startNode: Node<N>): List<Edge> {
+        val routes = startingRoutes(startNode)
+        while (!routes.minBy { it.weight() }.visited().containsAll(nodes)) {
+            bfsStep(
+                routes.minByOrNull { it.weight() } ?: listOf(),
+                routes
+            )
+        }
+
+        return routes.minBy { it.weight() }
+    }
+
+    fun maxBfs(startNode: Node<N>): List<Edge> {
+        val routes = startingRoutes(startNode)
+
+        while (!routes.maxBy { it.weight() }.visited().containsAll(nodes)) {
+            bfsStep(
+                routes.maxByOrNull { it.weight() } ?: listOf(),
+                routes
+            )
+        }
+
+        return routes.maxBy { it.weight() }
+    }
+
+    private fun startingRoutes(startNode: Node<N>): MutableList<List<Edge>> =
+        edges
+            .filter { it.from == startNode }
+            .map { listOf(it) }
+            .toMutableList()
+
+    private fun bfsStep(currentRoute: List<Edge>, routes: MutableList<List<Edge>>) {
+        val potentialEdges = currentRoute
+            .last()
+            .to
+            .let { lastVisited ->
+                edges.filter { it.from == lastVisited }
+            }.filterNot { currentRoute.visited().contains(it.to) }
+        for (potentialEdge in potentialEdges) {
+            if (!currentRoute.visited().contains(potentialEdge.to)) {
+                routes.add(currentRoute + potentialEdge)
+            }
+        }
+        routes.remove(currentRoute)
+
+    }
+
+    data class Node<T>(val id: T)
+
+    data class Edge(val from: Node<*>, val to: Node<*>, val weight: Long)
+}
+
+fun Collection<SearchGraph.Edge>.weight() = sumOf { it.weight }
+fun Collection<SearchGraph.Edge>.visited() = flatMap { setOf(it.from, it.to) }
--- src/net/sheltem/aoc/y2015/Day09.kt
+++ src/net/sheltem/aoc/y2015/Day09.kt
@@ -1,90 +1,38 @@
 package net.sheltem.aoc.y2015
 
-typealias Route = List<Day09.DistanceGraph.Edge>
+import net.sheltem.aoc.common.SearchGraph
+import net.sheltem.aoc.common.SearchGraph.Edge
+import net.sheltem.aoc.common.SearchGraph.Node
+import net.sheltem.aoc.common.weight
 
 suspend fun main() {
     Day09().run()
 }
 
 class Day09 : Day<Long>(605, 982) {
-    override suspend fun part1(input: List<String>): Long = DistanceGraph.from(input).let { graph ->
+    override suspend fun part1(input: List<String>): Long = input.toSearchGraph().let { graph ->
         graph.nodes
             .map { graph.minBfs(it) }
-    }.minOf { it.distance() }
+    }.minOf { it.weight() }
 
-    override suspend fun part2(input: List<String>): Long = DistanceGraph.from(input).let { graph ->
+    override suspend fun part2(input: List<String>): Long = input.toSearchGraph().let { graph ->
         graph.nodes
             .map { graph.maxBfs(it) }
-    }.maxOf { it.distance() }
-
-    class DistanceGraph(val nodes: Set<Node>, val edges: Set<Edge>) {
-
-        override fun toString() = "Nodes: $nodes\nEdges: $edges"
-
-        fun minBfs(startNode: Node): Route {
-            val routes = edges.filter { it.from == startNode }.map { listOf(it) }.toMutableList()
-
-            while (!routes.minBy { it.distance() }.visited().containsAll(nodes)) {
-                val shortest = routes.minByOrNull { it.distance() } ?: listOf()
-                bfsStep(shortest, routes)
-            }
-
-            return routes.minBy { it.distance() }
-        }
-
-        fun maxBfs(startNode: Node): Route {
-            val routes = edges.filter { it.from == startNode }.map { listOf(it) }.toMutableList()
-
-            while (!routes.maxBy { it.distance() }.visited().containsAll(nodes)) {
-                val longest = routes.maxByOrNull { it.distance() } ?: listOf()
-                bfsStep(longest, routes)
-            }
-
-            return routes.maxBy { it.distance() }
-        }
-
-        private fun bfsStep(
-            longest: List<Edge>,
-            routes: MutableList<List<Edge>>
-        ) {
-            val potentialEdges = longest
-                .last()
-                .to
-                .let { lastVisited ->
-                    edges.filter { it.from == lastVisited }
-                }.filterNot { longest.visited().contains(it.to) }
-            for (potentialEdge in potentialEdges) {
-                if (!longest.visited().contains(potentialEdge.to)) {
-                    routes.add(longest + potentialEdge)
-                }
-            }
-            routes.remove(longest)
-        }
-
-        data class Node(val name: String) {
-            override fun toString() = name
-        }
-
-        data class Edge(val from: Node, val to: Node, val distance: Long) {
-            override fun toString() = "$from -> $to | $distance"
-        }
+    }.maxOf { it.weight() }
 
 
-        companion object {
-            fun from(input: List<String>): DistanceGraph {
-                val nodes = mutableSetOf<Node>()
-                val edges = mutableSetOf<Edge>()
-                input.forEach { routeDescription ->
-                    val distance = routeDescription.split(" = ").last().toLong()
-                    val fromTo = routeDescription.split(" = ").first().split(" to ").take(2).map { Node(it) }
-                    nodes += fromTo
-                    edges += Edge(fromTo.component1(), fromTo.component2(), distance)
-                }
-                return DistanceGraph(nodes, edges + edges.map { Edge(it.to, it.from, it.distance) })
-            }
-        }
-    }
 }
 
-private fun Route.distance() = sumOf { it.distance }
-private fun Route.visited() = flatMap { setOf(it.from, it.to) }
+private fun List<String>.toSearchGraph(): SearchGraph<String> {
+    val nodes = mutableSetOf<Node<String>>()
+    val edges = mutableSetOf<Edge>()
+    this.forEach { routeDescription ->
+        val distance = routeDescription.split(" = ").last().toLong()
+        val fromTo = routeDescription.split(" = ").first().split(" to ").take(2).map { Node(it) }
+        nodes += fromTo
+        edges += Edge(fromTo.component1(), fromTo.component2(), distance)
+    }
+
+    return SearchGraph(nodes, edges, true)
+}
+
