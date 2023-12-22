3ec81be6bb7c010e3bc10d4e6327c16a3371f0a8
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2023-12-22 16:14:14 +0100

    Comment for crossinline

--- src/net/sheltem/aoc/common/Utils.kt
+++ src/net/sheltem/aoc/common/Utils.kt
@@ -3,6 +3,11 @@ package net.sheltem.aoc.common
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
+/*
+The crossinline marker is used to mark lambdas that mustn’t allow non-local returns,
+especially when such lambda is passed to another execution context such as a higher order function that is not inlined,
+a local object or a nested function. In other words, you won’t be able to do a return in such lambdas.
+ */
 suspend inline fun <T, R> Iterable<T>.mapParallel(crossinline transform: (T) -> R): List<R> = coroutineScope {
     map { async { transform(it) } }.map { it.await() }
 }
