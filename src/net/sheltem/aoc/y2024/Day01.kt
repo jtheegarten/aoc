6d48bfd3c06edbcdd14c5f83b90e0a707202ac8d
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2024-12-01 13:08:40 +0100

    small cleanup

--- src/net/sheltem/aoc/y2024/Day01.kt
+++ src/net/sheltem/aoc/y2024/Day01.kt
@@ -14,16 +14,26 @@ class Day01 : Day<Long>(11, 31) {
     override suspend fun part2(input: List<String>): Long = input.toSimilaritiesSum()
 }
 
-private fun List<String>.toDistances() = this.toSortedPairs().map { (it.first - it.second).absoluteValue }
+private fun List<String>.toDistances() =
+    toSortedPairs()
+        .map { (it.first - it.second).absoluteValue }
 
-private fun List<String>.toSimilaritiesSum() = this.toNumberPairs().unzip().let { (left, right) ->
-    left.fold(0L) { acc, lNumber ->
-        acc + (lNumber * right.count { it == lNumber })
-    }
-}
+private fun List<String>.toSimilaritiesSum() =
+    toNumberPairs()
+        .unzip()
+        .let { (left, right) ->
+            left.fold(0L) { acc, lNumber ->
+                acc + (lNumber * right.count { it == lNumber })
+            }
+        }
 
-private fun List<String>.toSortedPairs(): List<Pair<Long, Long>> = this.toNumberPairs().unzip().let {
-    it.first.sorted().zip(it.second.sorted())
-}
+private fun List<String>.toSortedPairs(): List<Pair<Long, Long>> =
+    toNumberPairs()
+        .unzip()
+        .let { (first, second) ->
+            first.sorted().zip(second.sorted())
+        }
 
-private fun List<String>.toNumberPairs() = map { it.regexNumbers() }.map { it.first() to it.last() }
+private fun List<String>.toNumberPairs() =
+    map { it.regexNumbers() }
+        .map { it.first() to it.last() }
