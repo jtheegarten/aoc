0a5af25ca72555401008078e39a47e2f6c0752a7
Jon Theegarten <jon.theegarten@iits-consulting.de>
2024-12-03 10:28:24 +0100

    cleanup

--- src/net/sheltem/aoc/y2024/Day03.kt
+++ src/net/sheltem/aoc/y2024/Day03.kt
@@ -11,18 +11,10 @@ class Day03 : Day<Long>(161, 48) {
 
     override suspend fun part1(input: List<String>): Long = input.joinToString().regex(mulRegex).doMults()
 
-    override suspend fun part2(input: List<String>): Long = input.joinToString().cutDonts().regex(mulRegex).doMults()
+    override suspend fun part2(input: List<String>): Long = input.joinToString().replace(cutRegex, "").regex(mulRegex).doMults()
 }
 
 private val mulRegex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
-private val startRegex = Regex(".*?don't\\(\\)")
-private val doRegex = Regex("(do\\(\\).*?don't\\(\\))|do\\(\\).*")
-
-private fun String.cutDonts(): String {
-    val start = startRegex.find(this)!!.value
-    val rest = doRegex.findAll(this.drop(start.length)).map { it.value }.onEach { println(it) }
-
-    return start.plus(rest.joinToString())
-}
+private val cutRegex = Regex("don't\\(\\).*?do\\(\\)")
 
 private fun List<String>.doMults(): Long = this.map { it.regexNumbers() }.sumOf { (first, second) -> first * second }
