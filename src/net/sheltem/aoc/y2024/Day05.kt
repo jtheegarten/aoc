a319b3fb7d4dfa17dc1473f29424372b882f4481
Jon Theegarten <jon.theegarten@iits-consulting.de>
2024-12-05 12:25:57 +0100

    2024 Day 5

--- src/net/sheltem/aoc/y2024/Day05.kt
+++ src/net/sheltem/aoc/y2024/Day05.kt
@@ -6,26 +6,43 @@ suspend fun main() {
     Day05().run()
 }
 
-class Day05 : Day<Long>(18, 9) {
+class Day05 : Day<Long>(143, 123) {
 
-    override suspend fun part1(input: List<String>): Long = input.parse().applyRules()
+    override suspend fun part1(input: List<String>): Long = input
+        .parse()
+        .let { (rules, pages) ->
+            pages
+                .filter { page -> page.isValid(rules) }
+                .sumOf { it.middle() }
+        }
 
-    override suspend fun part2(input: List<String>): Long = 9L
+    override suspend fun part2(input: List<String>): Long = input
+        .parse()
+        .let { (rules, pages) ->
+            pages.filter { page -> !page.isValid(rules) }
+                .map { it.sortPages(rules) }
+                .sumOf { it.middle() }
+        }
 }
 
-private fun List<String>.parse(): Pair<List<Rule>, List<List<Long>>> = indexOf("").let { this.take(it).map { Rule.from(it) } to this.drop(it + 1).map { it.regexNumbers() } }
-
-private fun Pair<List<Rule>, List<List<Long>>>.applyRules(): List<String> {
-    this.second.filter { it.apply { this.first } }
+private fun List<String>.parse(): Pair<List<Pair<Long, Long>>, List<List<Long>>> = indexOf("").let { splitIndex ->
+    this.take(splitIndex).map { rule ->
+        rule.regexNumbers().let { ruleNumbers -> ruleNumbers[0] to ruleNumbers[1] }
+    } to this.drop(splitIndex + 1).map { it.regexNumbers() }
 }
 
-private fun List<Long>.apply(rules: List<Rule>): Boolean = rules.all { rule -> rule.check(this) }
+private fun List<Long>.isValid(rules: List<Pair<Long, Long>>): Boolean = this.sortPages(rules) == this
 
-private class Rule(num: Long, prereq: Long) {
-    fun check(list: List<Long>): Boolean
+private fun List<Long>.sortPages(rules: List<Pair<Long, Long>>) = map { Page(it, rules) }.sorted().map { it.value }
 
-    companion object {
-        fun from(input: String): Rule =
-            input.regexNumbers().let { (pre, num) -> Rule(num, pre) }
+private fun List<Long>.middle(): Long = this[size / 2]
+
+private class Page(val value: Long, val rules: List<Pair<Long, Long>>) : Comparable<Page> {
+
+    override fun compareTo(other: Page): Int = when {
+        value == other.value -> 0
+        value to other.value in rules -> -1
+        else -> 1
     }
+
 }
