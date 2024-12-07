dbd8eac4231a8f0bfd7ab2d9c4f59fcf1425682f
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2024-12-07 14:06:03 +0100

    Averaging times and stuff

--- src/net/sheltem/common/Year.kt
+++ src/net/sheltem/common/Year.kt
@@ -6,7 +6,9 @@ import kotlin.io.path.Path
 import kotlin.io.path.writeText
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
 import kotlin.time.measureTime
+import kotlin.time.toDuration
 
 
 open class Year(
@@ -19,21 +21,21 @@ open class Year(
         val results = days
             .map { Triple(it.first, it.second, File("src/net/sheltem/aoc/y$year/data/Day${it.first}.txt").readLines()) }
             .map { (day, dayClass, input) ->
-                Triple(day, List(10) { measureTime { dayClass.part1(input) } }.min(), List(10) { measureTime { dayClass.part2(input) } }.min())
+                Triple(day, List(10) { measureTime { dayClass.part1(input) } }, List(10) { measureTime { dayClass.part2(input) } })
             }
 
         val formattedResults = results.map { (day, resultA, resultB) ->
-            listOf(day, resultA.formatted(), resultB.formatted(), (resultA + resultB).formatted())
+            listOf(day, resultA.min().formatted(), resultA.avg().formatted(), resultA.max().formatted(), resultB.min().formatted(), resultB.avg().formatted(), resultB.max().formatted(), (resultA.min() + resultB.min()).formatted(), (resultA.avg() + resultB.avg()).formatted(), (resultA.max() + resultB.max()).formatted())
         }
 
         val markdownTable = buildString {
 
-            val maxWidths = formattedResults.fold(listOf(0, 0, 0, 0)) { acc, row ->
+            val maxWidths = formattedResults.fold(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)) { acc, row ->
                 acc.zip(row.map { it.length }) { a, b -> maxOf(a, b) }
             }
 
             val headersLine =
-                listOf("Day", "Part A", "Part B", "Total")
+                listOf("Day", "Part A min", "avg", "max", "Part B min", "avg", "max", "Total min", "avg", "max")
                     .zip(maxWidths) { header, width -> header.padStart(width) }
                     .joinToString(" | ", prefix = "| ", postfix = " |")
             val separatorLine =
@@ -42,27 +44,45 @@ open class Year(
             appendLine(headersLine)
             appendLine(separatorLine)
 
-            formattedResults.forEach { (day, timeA, timeB, total) ->
-                val dayPadded = day.padEnd(maxWidths[0])
-                val timeAPadded = timeA.padStart(maxWidths[1])
-                val timeBPadded = timeB.padStart(maxWidths[2])
-                val totalPadded = total.padStart(maxWidths[3])
+            formattedResults.forEach {
+                val dayPadded = it[0].padEnd(maxWidths[0])
+                val timeAMinPadded = it[1].padStart(maxWidths[1])
+                val timeAAvgPadded = it[2].padStart(maxWidths[2])
+                val timeAMaxPadded = it[3].padStart(maxWidths[3])
+                val timeBMinPadded = it[4].padStart(maxWidths[4])
+                val timeBAvgPadded = it[5].padStart(maxWidths[5])
+                val timeBMaxPadded = it[6].padStart(maxWidths[6])
+                val totalMinPadded = it[7].padStart(maxWidths[7])
+                val totalAvgPadded = it[8].padStart(maxWidths[8])
+                val totalMaxPadded = it[9].padStart(maxWidths[9])
 
-                appendLine("| $dayPadded | $timeAPadded | $timeBPadded | $totalPadded |")
+                appendLine("| $dayPadded | $timeAMinPadded | $timeAAvgPadded | $timeAMaxPadded | $timeBMinPadded | $timeBAvgPadded | $timeBMaxPadded | $totalMinPadded | $totalAvgPadded | $totalMaxPadded |")
             }
         }
 
-        val totalTime = results.fold(0.seconds) { acc, (_, timeA, timeB) ->
-            acc + timeA + timeB
+
+
+        val totalTimeMin = results.fold(0.seconds) { acc, (_, timeA, timeB) ->
+            acc + timeA.min() + timeB.min()
         }
 
-        println("Total time: $totalTime")
+        val totalTimeAvg = results.fold(0.seconds) { acc, (_, timeA, timeB) ->
+            acc + timeA.avg() + timeB.avg()
+        }
+
+        val totalTimeMax = results.fold(0.seconds) { acc, (_, timeA, timeB) ->
+            acc + timeA.max() + timeB.max()
+        }
+
+        println("Total time: $totalTimeMin (min), $totalTimeAvg (avg), $totalTimeMax (max)")
 
         val outputPath = Path("TIMES$year.MD")
 
-        outputPath.writeText("$markdownTable\n\nTotal time: $totalTime\n")
+        outputPath.writeText("$markdownTable\n\n\"Total time: $totalTimeMin (min), $totalTimeAvg (avg), $totalTimeMax (max)\"\n")
     }
 }
+
+private fun List<Duration>.avg(): Duration = (sumOf { it.inWholeMicroseconds } / size).toDuration(DurationUnit.MICROSECONDS)
 
 private fun Duration.formatted(): String {
     val seconds = inWholeMicroseconds.toDouble() / 1000_000.0
