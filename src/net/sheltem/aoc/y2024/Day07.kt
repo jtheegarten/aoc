f524b3ba9e7c9a7aa3d0658c21c1d292d6140c5d
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2024-12-07 12:32:34 +0100

    Let's take the min() of 10 executions for our table. :3

--- src/net/sheltem/aoc/y2024/Day07.kt
+++ src/net/sheltem/aoc/y2024/Day07.kt
@@ -25,7 +25,7 @@ private fun List<Long>.calibrate(part2: Boolean = false): Long = this
 
 private fun List<Long>.combine(current: Long, goal: Long, part2: Boolean): Boolean = when {
     this.isEmpty() -> current == goal
-    this[0] > goal -> false
+    current > goal -> false
     this.drop(1).combine(current + this[0], goal, part2) -> true
     this.drop(1).combine(current * this[0], goal, part2) -> true
     part2 && this.drop(1).combine((current.toString() + this[0].toString()).toLong(), goal, part2) -> true
