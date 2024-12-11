c4e765a9111521530a2f58c6d60bcad82e090035
Jon Theegarten <jon.theegarten@iits-consulting.de>
2024-12-11 10:49:37 +0100

    small refactor

--- src/net/sheltem/aoc/y2024/Day11.kt
+++ src/net/sheltem/aoc/y2024/Day11.kt
@@ -8,20 +8,16 @@ suspend fun main() {
 
 class Day11 : Day<Long>(55312, 65601038650482) {
 
-    override suspend fun part1(input: List<String>): Long = input.single().regexNumbers().blink(25)
+    override suspend fun part1(input: List<String>): Long = input.single().regexNumbers().sumOf { it.blink(25) }
 
-    override suspend fun part2(input: List<String>): Long = input.single().regexNumbers().blink(75)
+    override suspend fun part2(input: List<String>): Long = input.single().regexNumbers().sumOf { it.blink(75) }
 
     private val cache = mutableMapOf<Pair<Long, Int>, Long>()
 
     private fun String.splitInHalf(): List<String> =
-        (length / 2).let { mid -> listOf(substring(0, mid), substring(mid)) }
+        (length / 2).let { mid -> listOf(this.take(mid), this.drop(mid)) }
 
-    private fun List<Long>.blink(times: Int): Long = this.sumOf {
-        it.blink(times)
-    }
-
-    private tailrec fun Long.blink(times: Int): Long = when {
+    private fun Long.blink(times: Int): Long = when {
         cache.contains(this to times) -> cache[this to times]!!
         times == 0 -> {
             cache[this to 0] = 1L
