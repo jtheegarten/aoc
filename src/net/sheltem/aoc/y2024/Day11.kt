b8822c6fe60662e8a6557feb81a743998fcada04
Jon Theegarten <jon.theegarten@iits-consulting.de>
2024-12-11 10:52:45 +0100

    small refactor

--- src/net/sheltem/aoc/y2024/Day11.kt
+++ src/net/sheltem/aoc/y2024/Day11.kt
@@ -19,23 +19,15 @@ class Day11 : Day<Long>(55312, 65601038650482) {
 
     private fun Long.blink(times: Int): Long = when {
         cache.contains(this to times) -> cache[this to times]!!
-        times == 0 -> {
-            cache[this to 0] = 1L
-            1L
-        }
+        times == 0 -> 1L.also { cache[this to 0] = it }
+        this == 0L -> 1L.blink(times - 1)
+            .also { cache[0L to times] = it }
 
-        this == 0L -> {
-            1L.blink(times - 1)
-                .also { cache[0L to times] = it }
-        }
+        this.toString().length % 2 == 0 -> this.toString()
+            .splitInHalf()
+            .map { it.toLong() }
+            .sumOf { it.blink(times - 1) }.also { cache[this to times] = it }
 
-        this.toString().length % 2 == 0 -> {
-            val (left, right) = this.toString().splitInHalf().map { it.toLong() }
-            (left.blink(times - 1) + right.blink(times - 1)).also { cache[this to times] = it }
-        }
-
-        else -> {
-            (this * 2024).blink(times - 1)
-        }
+        else -> (this * 2024).blink(times - 1)
     }
 }
