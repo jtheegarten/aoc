eced8962e7874cfc7a67ed5ed68dbe97a1702487
Jon Theegarten <jon.theegarten@iits-consulting.de>
2024-12-12 12:46:19 +0100

    more nesting please

--- src/net/sheltem/aoc/y2024/Day12.kt
+++ src/net/sheltem/aoc/y2024/Day12.kt
@@ -16,25 +16,26 @@ class Day12 : Day<Long>(1930, 1206) {
     override suspend fun part2(input: List<String>): Long = input.solve(true)
 
     private fun List<String>.solve(second: Boolean = false): Long {
-        val visited = mutableSetOf<PositionInt>()
-        return this.mapIndexed { y, row ->
-            row.mapIndexed { x, c ->
-                if (visited.contains(x to y)) 0L else {
-                    val plot = mutableSetOf<PositionInt>()
-                    val queue = ArrayDeque<PositionInt>()
-                    queue.add(x to y)
-                    var perimeter = 0
-                    while (queue.isNotEmpty()) {
-                        val pos = queue.removeFirst()
-                        if (!visited.add(pos)) continue
-                        plot.add(pos)
-                        val (inside, outside) = pos.neighbours().partition { this.charAtOrNull(it) == c }
-                        perimeter += if (second) this.corners(pos) else outside.size
-                        queue.addAll(inside)
+        mutableSetOf<PositionInt>().let { visited ->
+            return this.mapIndexed { y, row ->
+                row.mapIndexed { x, c ->
+                    if (visited.contains(x to y)) 0L else {
+                        val plot = mutableSetOf<PositionInt>()
+                        val queue = ArrayDeque<PositionInt>()
+                        queue.add(x to y)
+                        var perimeter = 0
+                        while (queue.isNotEmpty()) {
+                            val pos = queue.removeFirst()
+                            if (!visited.add(pos)) continue
+                            plot.add(pos)
+                            val (inside, outside) = pos.neighbours().partition { this.charAtOrNull(it) == c }
+                            perimeter += if (second) this.corners(pos) else outside.size
+                            queue.addAll(inside)
+                        }
+                        perimeter * plot.size.toLong()
                     }
-                    perimeter * plot.size.toLong()
-                }
+                }.sum()
             }.sum()
-        }.sum()
+        }
     }
 }
