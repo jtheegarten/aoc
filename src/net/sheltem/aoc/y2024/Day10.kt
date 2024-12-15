c5b149da24a9550320d65c063c643ac29643ffa0
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2024-12-16 00:06:51 +0100

    Day 10 with Grid

--- src/net/sheltem/aoc/y2024/Day10.kt
+++ src/net/sheltem/aoc/y2024/Day10.kt
@@ -1,9 +1,6 @@
 package net.sheltem.aoc.y2024
 
-import net.sheltem.common.PositionInt
-import net.sheltem.common.intAt
-import net.sheltem.common.neighbours
-import net.sheltem.common.withinArrayMap
+import net.sheltem.common.*
 
 suspend fun main() {
     Day10().run()
@@ -11,20 +8,20 @@ suspend fun main() {
 
 class Day10 : Day<Long>(36, 81) {
 
-    override suspend fun part1(input: List<String>): Long = input.map { it.toCharArray().map { char -> char.digitToInt() }.toTypedArray().toIntArray() }.trailheads().sumOf { it.toSet().size }.toLong()
+    override suspend fun part1(input: List<String>): Long = input.toGrid().transform { _, c -> c.digitToInt() }.trailheads().sumOf { it.toSet().size }.toLong()
 
-    override suspend fun part2(input: List<String>): Long = input.map { it.toCharArray().map { char -> char.digitToInt() }.toTypedArray().toIntArray() }.trailheads().sumOf { it.size }.toLong()
+    override suspend fun part2(input: List<String>): Long = input.toGrid().transform { _, c -> c.digitToInt() }.trailheads().sumOf { it.size }.toLong()
 
-    private fun List<IntArray>.trailheads(): List<List<PositionInt>> = indices
-        .flatMap { y -> this[y].indices.map { x -> x to y } }
-        .filter { this.intAt(it) == 0 }
-        .map { this.nines(it) }
+    private fun Grid<Int>.trailheads(): List<List<PositionInt>> =
+        allCoordinates
+            .filter { this[it] == 0 }
+            .map { this.nines(it) }
 
-    private fun List<IntArray>.nines(pos: PositionInt): List<PositionInt> = pos
-        .neighbours { next -> next.withinArrayMap(this) && (this.intAt(pos)!! + 1) == this.intAt(next) }
+    private fun Grid<Int>.nines(pos: PositionInt): List<PositionInt> = pos
+        .neighbours { next -> this.contains(next) && this[pos]!! + 1 == this[next] }
         .let { neighbours ->
             when {
-                this.intAt(pos) == 9 -> listOf(pos)
+                this[pos] == 9 -> listOf(pos)
                 neighbours.isEmpty() -> emptyList()
                 else -> neighbours.flatMap { this.nines(it) }
             }
