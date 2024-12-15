d6d71fe227c7fac356fbf4fc2110fcbb2c3d04f3
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2024-12-16 00:11:13 +0100

    Day 12 with Grid

--- src/net/sheltem/aoc/y2024/Day12.kt
+++ src/net/sheltem/aoc/y2024/Day12.kt
@@ -1,9 +1,6 @@
 package net.sheltem.aoc.y2024
 
-import net.sheltem.common.PositionInt
-import net.sheltem.common.charAtOrNull
-import net.sheltem.common.corners
-import net.sheltem.common.neighbours
+import net.sheltem.common.*
 
 suspend fun main() {
     Day12().run()
@@ -11,30 +8,29 @@ suspend fun main() {
 
 class Day12 : Day<Long>(1930, 1206) {
 
-    override suspend fun part1(input: List<String>): Long = input.solve()
+    override suspend fun part1(input: List<String>): Long = input.toGrid().solve()
 
-    override suspend fun part2(input: List<String>): Long = input.solve(true)
+    override suspend fun part2(input: List<String>): Long = input.toGrid().solve(true)
 
-    private fun List<String>.solve(second: Boolean = false): Long =
+    private fun Grid<Char>.solve(second: Boolean = false): Long =
         mutableSetOf<PositionInt>()
             .let { visited ->
-                this.mapIndexed { y, row ->
-                    row.mapIndexed { x, c ->
-                        if (visited.contains(x to y)) 0L else {
+                allCoordinates.sumOf { coord ->
+                        if (visited.contains(coord)) 0L else {
                             val plot = mutableSetOf<PositionInt>()
-                            val queue = ArrayDeque(listOf(x to y))
+                            val queue = ArrayDeque(listOf(coord))
                             var perimeter = 0
                             while (queue.isNotEmpty()) {
                                 val pos = queue.removeFirst()
                                 if (!visited.add(pos)) continue
                                 plot.add(pos)
-                                val (inside, outside) = pos.neighbours().partition { this.charAtOrNull(it) == c }
+                                val (inside, outside) = pos.neighbours().partition { this[it] == this[coord] }
                                 perimeter += if (second) this.corners(pos) else outside.size
                                 queue.addAll(inside)
                             }
                             perimeter * plot.size.toLong()
                         }
-                    }.sum()
-                }.sum()
+                    }
+                }
             }
-}
+
