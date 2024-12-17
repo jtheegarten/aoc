2b2af8a843dfec3f1acf5db89fa9e6ce0f30cbb4
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2024-12-17 11:54:00 +0100

    Current times

--- src/net/sheltem/aoc/y2024/Year2024.kt
+++ src/net/sheltem/aoc/y2024/Year2024.kt
@@ -19,7 +19,7 @@ private val days = listOf(
     "14" to Day14(),
     "15" to Day15(),
     "16" to Day16(),
-//    "17" to Day17(),
+    "17" to Day17(),
 //    "18" to Day18(),
 //    "19" to Day19(),
 //    "20" to Day20(),
