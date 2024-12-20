eb9ff59417b98c9fd016ec0b52d1454b5946b7bb
jtheegarten-iits <jon.theegarten@iits-consulting.de>
2024-12-20 11:48:47 +0100

    Fresh times

--- src/net/sheltem/aoc/y2024/Day20.kt
+++ src/net/sheltem/aoc/y2024/Day20.kt
@@ -1,8 +1,6 @@
 package net.sheltem.aoc.y2024
 
-import net.sheltem.common.Grid
-import net.sheltem.common.manhattan
-import net.sheltem.common.toGrid
+import net.sheltem.common.*
 
 suspend fun main() {
     Day20().run(true)
@@ -14,18 +12,18 @@ class Day20 : Day<Long>(6, 16) {
 
     override suspend fun part2(input: List<String>): Long = input.toGrid().findCheats(100, 20)
 
-    private fun Grid<Char>.findCheats(saved: Int, steps: Int): Long {
+    private suspend fun Grid<Char>.findCheats(saved: Int, steps: Int): Long {
         val start = this.find('S')
         val end = this.find('E')
 
         val path = this.dijkstra(start, end, { this.contains(it) && this[it] != '#' })
 
-        return path!!.indices.sumOf { i ->
+        return path!!.indices.sumOfParallel { i ->
             val j = i + saved + 1
             (j..path.lastIndex).count {
                 val distance = path[i].manhattan(path[it])
                 distance <= steps.toLong() && it - i - distance >= saved
-            }
-        }.toLong()
+            }.toLong()
+        }
     }
 }
